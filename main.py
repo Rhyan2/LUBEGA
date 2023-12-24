5e32a507431d8a1693914b41c0a90c8481afde6a
Utman <148863405+Rhyan2@users.noreply.github.com>
2023-12-24 14:05:44 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+NEWS_API_KEY = os.getenv("API_KEY")
+
+@app.route('/')
+def home():
+    
+    api_url = f'https://newsapi.org/v2/everything?q=cancer&apiKey={NEWS_API_KEY}'
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        data = response.json()
+        articles = data.get("articles", [])
+        return render_template('index.html', articles=articles)
+    else:
+        return f"Error fetching news from the API: {response.status_code}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
